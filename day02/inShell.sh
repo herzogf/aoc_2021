2350814f3ee13807e5a201a5d21469003852e789
Florian <herzog-florian@gmx.de>
2021-12-02 22:10:28 +0100

    Update inShell.sh

--- day02/inShell.sh
+++ day02/inShell.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# first puzzle
 cat aoc_02.txt | sed -e 's/forward \(.*\)/((forwardCounter+=\1))/g' -e 's/down \(.*\)/((depthCounter+=\1))/g' -e 's/up \(.*\)/((depthCounter-=\1))/g' | cat - <(echo 'echo $((forwardCounter*depthCounter))') | bash -
+
+# second puzzle
+cat aoc_02.txt | sed -e 's/forward \(.*\)/((forwardCounter+=\1));((depthCounter+=aimCounter*\1))/g' -e 's/down \(.*\)/((aimCounter+=\1))/g' -e 's/up \(.*\)/((aimCounter-=\1))/g' | cat - <(echo 'echo $((forwardCounter*depthCounter))') | bash -
